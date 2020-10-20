9ba7bb271ede303a5cb9dddd52fb8ddd69fc129a
MSI\moneo <jvr.moneo@gmail.com>
2020-10-20 14:52:09 +0200

    ngIf else

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  dni:string = 'Introduce tu dni';
+  dni:string;
   inputValueKeyUp:string ='';
 
   getDni() {
